e90c088fb7b173f0077be1c8ed654c2b300ae46d
yghjava <948170910@qq.com>
2017-05-30 22:37:42 +0800

    commit

--- week00_point_code1/code.c
+++ week00_point_code1/code.c
@@ -10,12 +10,12 @@
  *	gcc code.c -o code -m32
  *	the flowing program run result is:
  *		 sizeof(char)=1
-		 sizeof(short)=2
-		 sizeof(int)=4
-		 sizeof(long)=4
-		 sizeof(long long)=4
+ sizeof(short)=2
+ sizeof(int)=4
+ sizeof(long)=4
+ sizeof(long long)=4
 
-	If we want to complier at 32 environment,we need add such
+ If we want to complier at 32 environment,we need add such
  *	parameter
  *	gcc code.c -o code -m32 or gcc code.c -o code
  *	Because my system is 64,so the parameter can not add
@@ -28,13 +28,63 @@
  */
 #include<stdio.h>
 
-int main() {
+void fun1() {
 	printf("sizeof(char)=%ld\n", sizeof(char));
 	printf("sizeof(short)=%ld\n", sizeof(short));
 	printf("sizeof(int)=%ld\n", sizeof(int));
 	printf("sizeof(long)=%ld\n", sizeof(long));
 	printf("sizeof(long long)=%ld\n", sizeof(long));
-	printf("just test");
+}
+
+void fun2() {
+	int a;
+	a = 6;
+	printf("sizeof(int)=%ld\n", sizeof(int));
+	printf("sizeof(a)=%ld\n", sizeof(a + 1.0));
+	printf("a=%d\n", a);
+}
+
+void fun3() {
+	unsigned char a = 255;
+	printf("a=%d", a);
+}
+
+void fun4() {
+	int a = 1;
+	while (a > 0) {
+		a = a + 1;
+	}
+	a = a - 1;
+	printf("int的范围:%d\n", a);
+}
+
+void fun5() {
+	unsigned int a = 1;
+	long b;
+	while (a != 0) {
+		a = a + 1;
+	}
+	b = a-1;
+	printf("unsigned int的范围:%ld\n", b);
+}
+
+void fun6(){
+	int a = 012;
+	int b = 0x12a;
+	printf("a=%d,b=%d\n",a,b);
+	printf("a=%o,b=%x\n",a,b);
+}
+
+void fun7(){
+	int a=-1;
+	char b = -1;
+	printf("a=%u,b=%u",a,b);
+}
+
+int main() {
+//	fun2();
+//	fun1();
+	fun7();
 	return 0;
 }
 
