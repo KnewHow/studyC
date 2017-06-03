1e8361608354f12a7b99e4194c668545188aa588
yghjava <948170910@qq.com>
2017-06-03 17:41:29 +0800

    commit

---
+++ week00_string_code01/code.c
@@ -0,0 +1,40 @@
+/*
+ * code.c
+ *
+ *  Created on: 2017年6月3日
+ *      Author: ygh
+ *  A code to show the string ic C
+ */
+
+#include <stdio.h>
+
+void fun1(){
+	int i;
+	char *s="Hello!";
+	char *s2="Hello!";
+	printf("i address:%p\n",&i);
+	printf("s address:%p\n",s);
+	printf("s2 address:%p\n",s2);
+//	s[0]='B';
+//	printf("s[0]=%c",s[0]);
+
+}
+
+void fun2(){
+	int i;
+	char *s="Hello!";
+	char *s2="Hello!";
+	char s3[]="Hello!";
+	printf("i address:%p\n",&i);
+	printf("s address:%p\n",s);
+	printf("s2 address:%p\n",s2);
+	printf("s3 address:%p\n",s3);
+	s3[0]='B';
+	printf("s3[0]=%c",s3[0]);
+
+}
+int main(){
+	fun2();
+	return 0;
+}
+
